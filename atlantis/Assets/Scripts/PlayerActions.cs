239e4739326d830b6aa62ad98fab2efc34623fd1
Toya Rosuello <victoria.rosuello@gmail.com>
2022-07-27 23:00:23 -0700

    Stop movement when stick is returned to default state

--- atlantis/Assets/Scripts/PlayerActions.cs
+++ atlantis/Assets/Scripts/PlayerActions.cs
@@ -1,17 +1,37 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerActions : MonoBehaviour
-{
+public class PlayerActions : MonoBehaviour {
+    Vector3 _moveInput;
+
+    public void Awake() {
+        _moveInput = Vector3.zero;
+    }
+
     public void Move(InputAction.CallbackContext context) {
-        Vector2 leftStickMovement = Gamepad.current.leftStick.ReadValue()* new Vector2(0.10f, 0.10f);
+        if (Gamepad.current.leftStick.CheckStateIsAtDefault())
+        {
+            _moveInput = Vector3.zero;
+            return;
+        }
 
-        Vector3 localPos = gameObject.transform.localPosition;
-        Vector3 newPos = new Vector3(localPos.x+leftStickMovement.x, localPos.y, localPos.z+leftStickMovement.y);
-        Debug.Log("OLD" + localPos + " NEW: " + newPos);
+        Vector2 leftStickMovement = Gamepad.current.leftStick.ReadValue();
 
-        gameObject.transform.Translate(newPos, Space.World);
+        float forwardVelocity = 5*leftStickMovement.x;
+        float rightVelocity = 5*leftStickMovement.y;
+        
+        Vector3 moveForward = new Vector3(forwardVelocity * Time.fixedDeltaTime, 0, 0);
+        Vector3 moveRight = new Vector3(0, 0 , rightVelocity * Time.fixedDeltaTime);
+        
+        _moveInput = moveForward+moveRight;
+    }
+
+    public void FixedUpdate() {
+        if (_moveInput != Vector3.zero) {
+            gameObject.transform.Translate(_moveInput);
+        }
     }
 }
